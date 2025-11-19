7c03486f3ccfd5b6a4d8ca476bc5f488428ab034
horacodeoficial-svg <horacodeoficial@gmail.com>
2025-11-19 14:48:07 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
 export const metadata = {
-  title: 'HORA CODE',
-  description: 'Geração automática de sites por IA'
-}
+  title: "HORA CODE",
+  description: "Gerador automático de sites por IA"
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
